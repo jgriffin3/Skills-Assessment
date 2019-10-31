df91c173cfc7ede3c662d5b079daedddfcbc5031
John Griffin <jgriffin3@harding.edu>
2019-10-31 01:34:58 -0500

    Added code to randomly select hero image

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
-  // write your scripts here
+document.addEventListener("DOMContentLoaded", function() {
+  var heroClasses = ["hero1", "hero2", "hero3"];
+
+  var selection = Math.floor(Math.random() * 3);
+
+  document.getElementById('hero').classList.add(heroClasses[selection]);
 });
