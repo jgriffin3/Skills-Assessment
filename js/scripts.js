9d9cc328e45aaeab093ca1231002b644d4793840
John Griffin <jgriffin3@harding.edu>
2019-10-31 17:18:54 -0500

    Restructured HTML and adjusted SASS
    Restructured the HTML structure layout for the collage portion and adjusted SASS accordingly. Broke JS into function calls.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var heroClasses = ["hero1", "hero2", "hero3"];
-
-  var selection = Math.floor(Math.random() * 3);
-
-  document.getElementById('hero').classList.add(heroClasses[selection]);
+ 	getRandomHeroImage();
 });
+
+// This function will generate a random integer between 0 and 3
+//   and then assign the class associated with that number
+function getRandomHeroImage() {
+	var heroClasses = ["hero1", "hero2", "hero3"];
+
+    var selection = Math.floor(Math.random() * 3);
+
+    document.getElementById('hero').classList.add(heroClasses[selection]);
+}
